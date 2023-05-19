c3d4b3823f0635edcbdf94c245b1f105a4ad9577
onlinehk <18662909+onlinehk@users.noreply.github.com>
2023-05-19 12:42:08 +0800

    update

--- php/push.sh
+++ php/push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+#aws ecr get-login-password --region ap-east-1 | docker login --username AWS --password-stdin 742297649564.dkr.ecr.ap-east-1.amazonaws.com
 docker build -t 742297649564.dkr.ecr.ap-east-1.amazonaws.com/docker-lamp:latest .
 docker push 742297649564.dkr.ecr.ap-east-1.amazonaws.com/docker-lamp:latest
-sleep 10
 aws ecs update-service --cluster lamp-cluster --service lamp3 --force-new-deployment --no-cli-pager
--- php/www/index.php
+++ php/www/index.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Online HK</title>
+        <title>Online.HK</title>
         <style>
             html, body {
                 height: 100%;
@@ -13,6 +13,6 @@
         </style>
     </head>
     <body>
-        <h1 style="text-align: center; position: relative; top: 50%; transform: translateY(-50%);">Hello Online HK</h1>
+        <h1 style="text-align: center; position: relative; top: 50%; transform: translateY(-50%);">Hello Online.HK (Version 4)</h1>
     </body>
 </html>
